a940d2e9d7d0362578d73ad1168c70c5386da518
EmiliaKruszewska <emikruszewskaa@gmail.com>
2020-07-16 14:46:17 +0200

    back to top arrow

--- scripts.js
+++ scripts.js
@@ -8,14 +8,14 @@ burgerIcon.addEventListener('click', function(){
     backgroundMenuColor.classList.toggle('active');
 })
 const header = document.querySelector('header');
-// const toTop = document.querySelector('.to-top');
+const toTop = document.querySelector('.to-top');
 
 window.addEventListener('scroll', () => {
     if(window.pageYOffset > 100){
-        // toTop.classList.add('active');
+        toTop.classList.add('active');
         header.classList.add('active');
     } else {
-        // toTop.classList.remove('active');
+        toTop.classList.remove('active');
         header.classList.remove('active');
     }
 })
